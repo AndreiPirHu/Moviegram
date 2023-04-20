f922cf424c9b433e90fb78e4655fc6e502203845
VladZGarcia <vladi.garcia@gmail.com>
2023-04-20 12:03:10 +0200

    second commit nav

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./cart.css"
+
+export const Cart = () => {
+    return (
+        <div className="cart">
+            <div>
+                <h1>Cart Items</h1>
+            </div>
+            <div className="cartitem">
+
+            </div>
+        </div>
+    )
+}
