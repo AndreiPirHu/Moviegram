efe18416fa28c367969b02b2fb46ede811c957ad
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2023-04-24 10:04:12 +0200

    lagt till en sök funktion

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
-import { Navbar } from './components/navbar'
-import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
-import { Shop } from './pages/shop/shop'
-import { Cart } from './pages/cart/cart'
+import { useState } from "react";
+import { Navbar } from "./components/navbar";
+import "./App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
+import GetMoviePosters from "./components/getPosters";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <Router>
         <Navbar />
+
         <Routes>
           <Route path="/" element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
+        <GetMoviePosters />
       </Router>
     </div>
   );
-};
+}
 
-export default App
+export default App;
