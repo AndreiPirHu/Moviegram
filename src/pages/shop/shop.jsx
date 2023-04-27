3c838ed0cdfbe5a8fa9a51fcda8a1d2127baf025
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2023-04-27 16:14:12 +0200

    genre funktion som hämtar olika genre, laggt till knappar

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -4,56 +4,51 @@ import { useEffect, useState } from "react";
 
 import GetMoviePosters from "../../components/getPosters";
 
-import { v4 as uuidv4 } from 'uuid';
-import { useDispatch, useSelector } from 'react-redux';
-import { setDoc, collection, db, doc } from '../../firebase';
-import { actions } from '../../features/cartitems';
-
-
+import { v4 as uuidv4 } from "uuid";
+import { useDispatch, useSelector } from "react-redux";
+import { setDoc, collection, db, doc } from "../../firebase";
+import { actions } from "../../features/cartitems";
 
 export const Shop = () => {
   const [movie, setMovie] = useState([]);
 
+  const user = useSelector((state) => state.login.user);
+  const cart = useSelector((state) => state.cartItems);
 
-    const user = useSelector( state => state.login.user)
-    const cart = useSelector(state => state.cartItems)
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const items = [
+    { id: uuidv4(), name: "Item 1", price: 10 },
+    { id: uuidv4(), name: "Item 2", price: 15 },
+    { id: uuidv4(), name: "Item 3", price: 20 },
+  ];
 
-
-    const items = [
-        { id: uuidv4(), name: 'Item 1', price: 10 },
-        { id: uuidv4(), name: 'Item 2', price: 15 },
-        { id: uuidv4(), name: 'Item 3', price: 20 },
-      ];
-
-    const addToCart = async (item) => {
-        //adds item to cart
+  const addToCart = async (item) => {
+    //adds item to cart
     dispatch(actions.addItem(item));
 
     //TODO add so it ONLY adds when user is logged in
     if (!user) {
-        console.log('user is not logged in for firestore save');
-        
-        return;
+      console.log("user is not logged in for firestore save");
+
+      return;
     }
-    try{
-        //reference to correct collection
-        const cartItemsRef = collection(db, 'users', user, 'cartItems');
+    try {
+      //reference to correct collection
+      const cartItemsRef = collection(db, "users", user, "cartItems");
 
-        // Set the itemID as the doc name
-        const itemDocRef = doc(cartItemsRef, item.id);
+      // Set the itemID as the doc name
+      const itemDocRef = doc(cartItemsRef, item.id);
 
-        //Add item to firestore
-        await setDoc(itemDocRef, item);
-        console.log(`Item added to firestore with ID: ${item.id}`);
+      //Add item to firestore
+      await setDoc(itemDocRef, item);
+      console.log(`Item added to firestore with ID: ${item.id}`);
     } catch (e) {
-        console.error('Error adding item to firestore:', e)
+      console.error("Error adding item to firestore:", e);
     }
-    
-    };
+  };
 
-const FetchMovies = () => {
+  const FetchMovies = () => {
     fetch(
       "https://api.themoviedb.org/3/trending/all/week?api_key=9bf8866aec070a01073c600a88bbefb5"
     )
@@ -70,8 +65,8 @@ const FetchMovies = () => {
   useEffect(() => {
     FetchMovies();
   }, []);
-    return (
-          <div className="shop">
+  return (
+    <div className="shop">
       <div className="shopTitle">
         <h1>Moviegram</h1>
         <GetMoviePosters />
@@ -84,21 +79,20 @@ const FetchMovies = () => {
               src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
               alt="Movie poster"
             />
-            <div className='testShop'>
-<h1>Items for Sale</h1>
-<ul>
-        {items.map((item) => (
-<li key={item.id}>
-            {item.name} - ${item.price}
-<button onClick={() => addToCart(item)}>Add to Cart</button>
-</li>
-        ))}
-</ul>
-</div>
+            <div className="testShop">
+              <h1>Items for Sale</h1>
+              <ul>
+                {items.map((item) => (
+                  <li key={item.id}>
+                    {item.name} - ${item.price}
+                    <button onClick={() => addToCart(item)}>Add to Cart</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         ))}
       </div>
     </div>
-    )
-}
-
+  );
+};
