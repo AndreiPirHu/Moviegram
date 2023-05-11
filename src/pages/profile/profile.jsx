18fd6b3e0a1aaf170b6b4c9f98039bc4efd89035
AndreiPirHu <andrei.pirogov.huertas@gmail.com>
2023-05-11 13:27:18 +0200

    Added function to remove cart from firestore Added dispatch to clear cart Fixed profile css Connected order history to firestore

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,41 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './profile.css'
-import { auth } from '../../firebase';
+import { auth, collection, db, getDocs } from '../../firebase';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { actions as cartActions} from '../../features/cartitems';
+import { actions as cartActions } from '../../features/cartitems';
 import { v4 as uuidv4 } from 'uuid';
 
-export const Profile = () =>{
-  
-  const isLoggedIn = useSelector( state => state.login.loggedIn );
-  const userInfo = useSelector( state => state.login.userInfo);
+export const Profile = () => {
+  const [orders, setOrders] = useState([]);
+  const isLoggedIn = useSelector(state => state.login.loggedIn);
+  const userInfo = useSelector(state => state.login.userInfo);
 
+  const userID = useSelector(state => state.login.user);
   const dispatch = useDispatch();
   let navigate = useNavigate();
 
 
-  const orders = [
-  {
-    id: uuidv4(),
-    date: '12 may',
-    items: {},
-    total: '£120',
-  },
-  {
-    id: uuidv4(),
-    date: '12 may',
-    items: {},
-    total: '£120',
+
+
+
+  const getOrderHistory = async () => {
+    const querySnapshot = await getDocs(collection(db, "users", userID, "orderHistory"));
+
+    const ordersData = querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        date: data.date,
+        id: data.id,
+        items: data.items,
+        price: data.price,
+      };
+    });
+
+    setOrders(ordersData)
   }
-  ]
 
   //sign out user using firebase auth
   //redirect to login page
   //clears cart for offline mode
   const handleSignOut = async () => {
     try {
-      await auth.signOut(); 
+      await auth.signOut();
       console.log('User signed out successfully');
       dispatch(cartActions.clearItems())
       navigate('/login');
@@ -43,6 +48,13 @@ export const Profile = () =>{
       console.log('Error signing out:', error);
     }
   };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      getOrderHistory()
+    }, 50);
+    return () => clearTimeout(timeoutId);
+  }, [])
 
   //redirects to new page if user logs in and if web address is written manually
   useEffect(() => {
@@ -53,57 +65,82 @@ export const Profile = () =>{
   }, [isLoggedIn]);
 
 
-return(
-<div className="profile">
-  <div className='user-info'>
-    
-  {userInfo ? (
-  <>
-    <h1>Welcome {userInfo.name}</h1>
-    <div className="user-details">
-      <ul>
-        <li>
-          <strong>Name:</strong> {userInfo.name}
-        </li>
-        <li>
-          <strong>Email:</strong> {userInfo.email}
-        </li>
-        <li>
-          <strong>Address:</strong> {userInfo.address}
-        </li>
-      </ul>
-    </div>
-  </>
-  ) : (
-    <p>Loading user information...</p>
-  )}
-      
-      <button>Edit Information</button>
-</div>
-<div className='order-details-info'>
-      <h2>Order History</h2>
-      <ul>
-        {orders.map(order => (
-          <div className='user-order' >
-            <li key={order.id}>
-            <div>Order number: {order.id}</div>
-            <div>Date: {order.date}</div>
-            <div>Items: </div>
-            <div>Total: ${order.total}</div>
-          </li>
-          </div>
-          
-        ))}
-      </ul>
+  return (
+    <div className="profile">
+      <div className='user-info'>
 
-      </div >
-      <div className='user-signout'>
+        {userInfo ? (
+          <>
+            <h1>Welcome {userInfo.name}</h1>
+            <div className="user-details">
+              <ul>
+                <li>
+                  <strong>Name:</strong> {userInfo.name}
+                </li>
+                <li>
+                  <strong>Email:</strong> {userInfo.email}
+                </li>
+                <li>
+                  <strong>Address:</strong> {userInfo.address}
+                </li>
+              </ul>
+            </div>
+          </>
+        ) : (
+          <p>Loading user information...</p>
+        )}
+
+        <button>Edit Information</button>
+        <div className='user-signout'>
         <button onClick={handleSignOut}>Log out</button>
       </div>
-          
-          
+      </div>
+      <div className='order-details-info'>
+        <h2>Order History</h2>
+        <ul>
+  {orders.map(order => (
+    <div className='user-order' key={order.id}>
+      <div className='order-number'>Order number: {order.id}</div>
+      <div className='order-date'>Date: {order.date}</div>
+      {(() => {
+        const groupedItems = {};
+        order.items.forEach(item => {
+          const key = `${item.name}-${item.size}-${item.price}`;
+          if (!groupedItems[key]) {
+            groupedItems[key] = {
+              item,
+              itemCount: 1
+            };
+          } else {
+            groupedItems[key].itemCount++;
+          }
+        });
+        return Object.values(groupedItems).map(({ item, itemCount }) => (
+          <div className='item-container' key={item.id}>
+            <div className='image-container'>
+              <img className='item-image' src={item.img} alt={item.name} />
+            </div>
+            <div className='item-details-container'>
+              <div className='item-name-container'>
+                <p className='item-name'>{item.name}</p>
+                <p className='item-size'>{item.size}</p>
+              </div>
+                <p className='item-amount'>{itemCount}</p>
+                <p className='item-price'>${item.price * itemCount}</p>
+              
+            </div>
+          </div>
+        ));
+      })()}
+      <div className='total-price'>Total: ${order.price}</div>
     </div>
-)
+  ))}
+</ul>
+
+      </div >
+
+    </div>
+  )
 
 
 }
